d500f6498697b5f5f949c8b795bbd61304d2640e
Paul McCrory <paul.mccrory@unosquare.com>
2020-10-20 17:30:14 +0100

    feature/PHASE2-867 - Add parent ul element to navigation list links

--- src/components/Layout/SideNavigation/index.tsx
+++ src/components/Layout/SideNavigation/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import NavigationLink from './NavigationLink';
-import NavigationContainer from './styled';
+import { NavigationContainer, NavigationList } from './styled';
 import { Navigation } from '../../../constants';
 
 const SideNavigation: React.FC = () => (
   <NavigationContainer>
-    <NavigationLink
-      pendingItems={false}
-      to={`${Navigation.NAVIGATION_ROUTES.HOME}`}
-    >
-      Home
-    </NavigationLink>
-    <NavigationLink to={`${Navigation.NAVIGATION_ROUTES.RATINGS_AND_REVIEWS}`}>
-      Ratings And Reviews
-    </NavigationLink>
-    <NavigationLink to={`${Navigation.NAVIGATION_ROUTES.ADDRESS}`}>
-      Address
-    </NavigationLink>
-    <NavigationLink to={`${Navigation.NAVIGATION_ROUTES.WEB_ACCESS}`}>
-      Web Access
-    </NavigationLink>
+    <NavigationList>
+      <NavigationLink
+        pendingItems={false}
+        to={`${Navigation.NAVIGATION_ROUTES.HOME}`}
+      >
+        Home
+      </NavigationLink>
+      <NavigationLink
+        to={`${Navigation.NAVIGATION_ROUTES.RATINGS_AND_REVIEWS}`}
+      >
+        Ratings And Reviews
+      </NavigationLink>
+      <NavigationLink to={`${Navigation.NAVIGATION_ROUTES.ADDRESS}`}>
+        Address
+      </NavigationLink>
+      <NavigationLink to={`${Navigation.NAVIGATION_ROUTES.WEB_ACCESS}`}>
+        Web Access
+      </NavigationLink>
+    </NavigationList>
   </NavigationContainer>
 );
 
--- src/components/Layout/SideNavigation/styled.ts
+++ src/components/Layout/SideNavigation/styled.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-const NavigationContainer = styled.nav`
+export const NavigationContainer = styled.nav`
   background-color: ${({ theme }) => theme.colors.lightGrey};
   border-right: 1px solid ${({ theme }) => theme.colors.borderMain};
   height: 90vh;
 `;
 
-export default NavigationContainer;
+export const NavigationList = styled.ul`
+  padding-inline-start: 0px;
+`;
