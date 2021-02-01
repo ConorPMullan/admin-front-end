174e88ef8e9da4e9af8cf15299364614dbe496ae
Christopher Hunter <christopher.hunter@unosquare.com>
2021-02-01 13:07:44 +0000

    Typo

--- src/components/Layout/Navigation/NavigationDrawer/NavigationList/NavigationList.test.tsx
+++ src/components/Layout/Navigation/NavigationDrawer/NavigationList/NavigationList.test.tsx
@@ -11,13 +11,13 @@ describe('NavigationList Tests', () => {
     const navList = getByTestId('nav-list');
     const navItemHome = getByTestId('nav-item-home');
     const navItemHomeIcon = getByTestId('nav-item-home-icon');
-    const navItemManageProduct = getByTestId('nav-item-manage-products');
-    const navItemManageProductIcon = getByTestId('nav-item-manage-products-icon');
+    const navItemManageProducts = getByTestId('nav-item-manage-products');
+    const navItemManageProductsIcon = getByTestId('nav-item-manage-products-icon');
     
     expect(navList).toBeInTheDocument();
     expect(navItemHome).toBeInTheDocument();
     expect(navItemHomeIcon).toBeInTheDocument();
-    expect(navItemManageProduct).toBeInTheDocument();
-    expect(navItemManageProductIcon).toBeInTheDocument();
+    expect(navItemManageProducts).toBeInTheDocument();
+    expect(navItemManageProductsIcon).toBeInTheDocument();
   });
 });
