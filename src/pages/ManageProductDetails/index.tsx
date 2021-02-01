44da9391f270b1b34e27009dcf70d6301211a29d
Christopher Hunter <christopher.hunter@unosquare.com>
2021-02-01 11:54:07 +0000

    Updated ManageProductDetails to match Home screen format

--- src/pages/ManageProductDetails/index.tsx
+++ src/pages/ManageProductDetails/index.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from 'react';
 
-const ProductDetails: React.FC = (): ReactElement => (
-  <div>Hello, I am the manage product details page!</div>
-);
+const ProductDetails: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div>Hello, I am the manage product details page!</div>
+    </div>
+  )
+};
 
 export default ProductDetails;
