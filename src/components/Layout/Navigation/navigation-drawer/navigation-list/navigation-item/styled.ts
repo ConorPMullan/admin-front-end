1bb6bca9b09947ec1a6c1563e09bfaa75abc8378
UnoJonnyKhattak <jonathan.khattak@unosquare.com>
2021-02-03 10:14:42 +0000

    Delete src/components/Layout directory

--- src/components/Layout/Navigation/navigation-drawer/navigation-list/navigation-item/styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-
-export const NavigationListItemContainer = styled.li``;
-export const NavigationListItemIcon = styled(ListItemIcon)``;
-export const NavigationListItemText = styled(ListItemText)``;
--- src/components/Layout/Navigation/navigation-drawer/navigation-list/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import List from '@material-ui/core/List';
-import Home from '@material-ui/icons/Home';
-import Edit from '@material-ui/icons/Edit';
-
-export const NavList = styled(List)``;
-export const IconHome = styled(Home)``;
-export const IconManageProducts = styled(Edit)``;
