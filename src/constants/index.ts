bd38b38fe13090ebc5ae2f5e0ff1f8eec24e22d0
UnoJonnyKhattak <jonathan.khattak@unosquare.com>
2021-02-17 16:57:59 +0000

    OPL2-43 - Removed uneccessary ValidationRegex

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,3 @@ export { default as Product } from './products';
 export { default as Login } from './login';
 export { default as Navigation } from './navigation';
 export { default as User } from './user';
-export { default as ValidationExpressions } from './validationExpressions';
--- src/constants/validationExpressions.ts
+++
@@ -1,9 +0,0 @@
-interface ValidationExpressionTypes {
-  PASSWORD: RegExp;
-}
-
-const VALIDATION_EXPRESSIONS: ValidationExpressionTypes = {
-  PASSWORD: /^(?=.*\d)(?=.*[A-Z]).{8,20}$/,
-};
-
-export default VALIDATION_EXPRESSIONS;
