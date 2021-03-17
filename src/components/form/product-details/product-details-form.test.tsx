9a7b87681da32a127e7d17df12b3fd8a04d3156f
UnoJonnyKhattak <jonathan.khattak@unosquare.com>
2021-03-17 17:26:33 +0000

    OPL2-144 - Added check for Product Details Form fields

--- src/components/form/product-details/product-details-form.test.tsx
+++ src/components/form/product-details/product-details-form.test.tsx
@@ -8,12 +8,20 @@ const tabLongDescription = 'product-form-long-description-tab';
 const tabIngredients = 'product-form-ingredients-tab';
 const tabGuaranteedAnalysis = 'product-form-guaranteed-analysis-tab';
 
+const fieldItemNumber = 'product-form-item-number';
+const fieldName = 'product-form-name';
+const fieldUpc = 'product-form-upc';
+const fieldUm = 'product-form-um';
+const fieldBrand = 'product-form-brand';
+const fieldSize = 'product-form-size';
+const fieldEdlp = 'product-form-edlp';
+
 const fieldLongDescription = 'product-form-long-description';
 const fieldIngredients = 'product-form-ingredients';
 const fieldGuaranteedAnalysis = 'product-form-guaranteed-analysis';
 
 describe('ProductDetailsForm Tests', () => {
-  test('default rendering of the ProductDetailsForm', async () => {
+  test('default rendering of the ProductDetailsForm shows details tab', async () => {
     const { getByTestId, queryByTestId } = TestUtils.render(
       <ProductDetailsForm />
     );
@@ -21,6 +29,7 @@ describe('ProductDetailsForm Tests', () => {
     expect(getByTestId(tabLongDescription)).toBeInTheDocument();
     expect(getByTestId(tabIngredients)).toBeInTheDocument();
     expect(getByTestId(tabGuaranteedAnalysis)).toBeInTheDocument();
+    expect(queryByTestId(fieldItemNumber)).toBeInTheDocument();
     expect(queryByTestId(fieldLongDescription)).toBeNull();
     expect(queryByTestId(fieldIngredients)).toBeNull();
     expect(queryByTestId(fieldGuaranteedAnalysis)).toBeNull();
